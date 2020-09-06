3e67b9078e02ddc79bbaa72acef4954c2374612d
Matthias Hecht <matthias@hecht.space>
2020-09-06 17:29:34 +0200

    feat: finishes first iteration MVP :tada: :tada:

---
+++ src/config.ts
@@ -0,0 +1,147 @@
+import * as fs from 'fs';
+
+export function load(path?: string): Config {
+  let config;
+  if (path) {
+    const stringConfig = fs.readFileSync(path, { encoding: 'utf-8' });
+    config = JSON.parse(stringConfig);
+  } else {
+    config = DEFAULT_CONFIG;
+  }
+
+  const [res, msg] = validateConfig(config);
+
+  if (!res) {
+    throw new Error('VALIDATION: ' + msg);
+  }
+
+  return config;
+}
+
+const DEFAULT_CONFIG = {
+  sections: [
+    {
+      key: 'Einsatzanlass',
+      type: 'keyValue'
+    },
+    {
+      key: 'Einsatzort',
+      type: 'try'
+    },
+    {
+      key: 'EM',
+      type: 'table'
+    }
+  ]
+};
+
+function validateConfig(config: any): [boolean, string] {
+  let result = true;
+  let msg = '';
+  Object.entries(ConfigAssertion).forEach(([key, val]) => {
+    [result, msg] = validateSubConfig(config[key], key, val);
+  });
+  return [result, msg];
+}
+
+function validateSubConfig(config: any, key: string, val: IConfigAssertionTypes): [boolean, string] {
+  if (val.required && !config) return [false, `Required property ${key} is missing`];
+
+  if (isPrimitive(val.type) && val.type !== typeof config) return [false, `Property ${key} is type ${typeof config} but should be type ${val.type}`];
+
+  if (val.type === 'array') {
+    if (!Array.isArray(config)) return [false, `Property ${key} is type object but should be type array`];
+
+    if (isPrimitive(val.children) && (config as any[]).find((v) => typeof v !== val.children))
+      return [false, `Property ${key} should only have children of type ${val.children}`];
+
+    if (!isPrimitive(val.children)) {
+      let [tmpRes, tmpMsg] = [true, ''];
+
+      (config as any[]).forEach((v) => {
+        Object.entries(val.children as IConfigAssertion).forEach(([subKey, val]) => {
+          [tmpRes, tmpMsg] = validateSubConfig(v[subKey], subKey, val);
+          if (!tmpRes) return [false, tmpMsg];
+          return [true, ''];
+        });
+        if (!tmpRes) return [false, tmpMsg];
+        return [true, ''];
+      });
+    }
+  }
+  if (val.type === 'object') {
+    if (typeof config !== 'object') return [false, `Property ${key} should be type object`];
+
+    let [tmpRes, tmpMsg] = [true, ''];
+
+    Object.entries(val.properties).forEach(([subKey, val]) => {
+      [tmpRes, tmpMsg] = validateSubConfig(config[subKey], subKey, val);
+      if (!tmpRes) return [false, tmpMsg];
+      return [true, ''];
+    });
+
+    if (!tmpRes) return [false, tmpMsg];
+  }
+
+  return [true, ''];
+}
+
+export interface Config {
+  sections: Section[];
+}
+
+export interface Section {
+  key: string;
+  type: 'keyValue' | 'table' | 'try';
+  tableUseRowHeadings: boolean;
+}
+
+const ConfigAssertion: IConfigAssertion = {
+  sections: {
+    required: true,
+    type: 'array',
+    children: {
+      key: {
+        required: true,
+        type: 'string'
+      },
+      type: {
+        required: true,
+        type: 'string' // is a enum
+      }
+    }
+  }
+};
+
+interface IConfigAssertion {
+  [key: string]: IConfigAssertionTypes;
+}
+
+type IConfigAssertionTypes = IConfigAssertionPrimitive | IConfigAssertionArray | IConfigAssertionObject;
+
+type primitive = 'string' | 'number' | 'bigint' | 'boolean';
+const isPrimitive = (type: any): type is primitive => ['string', 'number', 'bigint', 'boolean'].includes(type);
+
+type types = primitive | 'object' | 'array';
+
+interface IConfigAssertionsBase {
+  required: boolean;
+  type: types;
+}
+
+interface IConfigAssertionPrimitive extends IConfigAssertionsBase {
+  required: boolean;
+  type: primitive;
+}
+
+interface IConfigAssertionArray extends IConfigAssertionsBase {
+  required: boolean;
+  type: 'array';
+  children: IConfigAssertion | primitive;
+}
+
+interface IConfigAssertionObject extends IConfigAssertionsBase {
+  required: boolean;
+  type: 'object';
+  properties: IConfigAssertion;
+}
---
+++ src/const.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+import path = require('path');
+
+namespace CONST {
+  // Make log dir
+  if (!fs.existsSync('./logs')) fs.mkdirSync('./logs');
+  export const LOG_PATH = `${path.resolve('./logs')}/${new Date().toISOString().replace(/^(.*)T.*/, '$1')}.log`;
+
+  export const INTERNAL_ERROR = `
+An internal error occured!
+Please check the log file for more information!
+Path: ${LOG_PATH}
+`;
+
+  export const HELP_MESSAGE = `
+VERSION ${process.env.npm_package_version || 'dev system'}
+Extractor tool of EMAPC (EinsatzMonitor-Alarm-PDF-Converter).
+MIT Licensed.
+Written by Matthias Hecht (https://github.com/matzehecht).
+Source available: https://github.com/matzehecht/EinsatzMonitor-Alarm-PDF-Converter
+
+Documentation: https://github.com/matzehecht/EinsatzMonitor-Alarm-PDF-Converter/README.md
+
+This tool extracts the relevant information of the alarm pdf into a key-value formated file.
+
+USAGE: extractor [--config configFilePath] inputFile outFileOrDir
+
+MANDATORY ARGUMENTS:
+    inputFile           This has to be the path to a valid PDF file. (The alarm pdf file)
+    outFileOrDir        This can be the path to a (not-existing) output file. 
+                            In this case the script will use the provided file name.
+                            - OR -
+                        This can be the path to a existing directory.
+                            In this case the script will auto generate the file name.
+
+OPTIONAL ARGUMENTS:
+    --config            This parameter should be followed by a valid path the a custom config file.
+                        If not provided the script will use the default config.`;
+}
+
+export default CONST;
