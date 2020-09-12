daea67902b7c9fbab35508e3a8b672cbae507dba
Matthias Hecht <matthias@hecht.space>
2020-09-12 19:18:43 +0200

    refactor(config): better config handler

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+import { createValidator } from '@typeonly/validator';
+import * as YAML from 'yamljs';
+import { utils } from '../utils';
+import { Config, Section } from './config';
+export { Config, Section };
+
+export function load(configPath: string = './default.conf.yml'): Config {
+  const config = YAML.load(configPath);
+
+  const validator = createValidator({
+    bundle: require('./config-types.to.json')
+  });
+
+  const result = validator.validate('Config', config);
+
+  if (!result.valid) {
+    throw new Error(result.error);
+  } else {
+    utils.logInfo('CONFIG', 'validated config successfully!');
+  }
+
+  return config;
+}
