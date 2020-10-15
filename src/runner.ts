e6c0cb4f18c0d592e41427dc1d41f1092df9c3fb
Matthias Hecht <matthias@hecht.space>
2020-10-15 23:48:40 +0200

    refactor(performance): updates trace-it

--- src/runner.ts
+++ src/runner.ts
@@ -2,20 +2,24 @@ import * as chokidar from 'chokidar';
 import { Stats, promises as fsPromises } from 'fs';
 import * as path from 'path';
 import * as TraceIt from 'trace-it';
+import { LowDbAdapter } from '@trace-it/lowdb-adapter';
 
 import { convert } from '.';
 import { load as loadConfig } from './config';
 import * as utils from './utils';
 
-TraceIt.init({ storage: 'lowdb', storageOptions: { dbName: './tmp/perf.json' } });
+const shouldTrace = Boolean(process.env.SHOULD_TRACE) || false;
+
+const adapter = shouldTrace ? new LowDbAdapter({ dbName: './tmp/perf.json' }) : undefined;
+if (shouldTrace) TraceIt.init(adapter as LowDbAdapter);
 
 const CONFIG_FILE = path.resolve('./emapc.conf.yml');
 
-const configTransaction = TraceIt.startTransaction('loadConfig');
-configTransaction.set('path', CONFIG_FILE);
+const configTransaction = shouldTrace ? TraceIt.startTransaction('loadConfig') : undefined;
+configTransaction?.set('path', CONFIG_FILE);
 const config = loadConfig(CONFIG_FILE);
-configTransaction.set('config', config);
-configTransaction.end();
+configTransaction?.set('config', config);
+configTransaction?.end();
 
 if (!config.runner) utils.throwErr(new Error('CONFIG - runner config missing!'));
 
@@ -27,14 +31,14 @@ const watcher = chokidar.watch(`${utils.unixPathFrom(config.runner!.inputDir)}/*
 watcher.on('add', run);
 
 async function run(filepath: string, stat?: Stats) {
-  const fileChangeTransaction = TraceIt.startTransaction('file change detected');
-  fileChangeTransaction.set('filepath', filepath);
+  const fileChangeTransaction = shouldTrace ? TraceIt.startTransaction('file change detected') : undefined;
+  fileChangeTransaction?.set('filepath', filepath);
   await convert(filepath, false, config.runner?.outputDir as string, config, fileChangeTransaction);
 
   if (config.runner?.archiveDir) {
-    const archiveTransaction = fileChangeTransaction.startChild('archive');
+    const archiveTransaction = fileChangeTransaction?.startChild('archive');
     await fsPromises.rename(path.resolve(filepath), path.resolve(config.runner?.archiveDir as string, path.basename(filepath)));
-    archiveTransaction.end();
+    archiveTransaction?.end();
   }
-  fileChangeTransaction.end();
+  fileChangeTransaction?.end();
 }
