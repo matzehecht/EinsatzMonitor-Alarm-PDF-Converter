7e7f6f9e1e80ea26cf7dcfa61e521f7c905ca140
Matthias Hecht <matthias@hecht.space>
2020-10-24 01:22:29 +0200

    fix: fixes the fallback mode

--- src/cli.ts
+++ src/cli.ts
@@ -21,7 +21,7 @@ function parseArgs(argv: string[]) {
 
   // get config
   const configIndex = args.findIndex((a) => ['--config', '-c'].includes(a));
-  if (configIndex === -1) utils.throwErr(new Error('CONFIG - no config provided'));
+  if (configIndex === -1) throwErr(new Error('CONFIG - no config provided'));
   const configFile = args[configIndex + 1];
 
   // remove '--config' and the following filepath from the array
@@ -30,38 +30,38 @@ function parseArgs(argv: string[]) {
   utils.logInfo(!configFile ? 'Using standard config' : `using config file ${configFile}`);
 
   const inputFileOrDir = args[0];
-  if (!inputFileOrDir) utils.throwErr(new Error('No input given!'));
+  if (!inputFileOrDir) throwErr(new Error('No input given!'));
   utils.logInfo('INPUT', 'path ' + inputFileOrDir);
   const outputArg = args[1];
-  if (!outputArg) utils.throwErr(new Error('No output path given!'));
+  if (!outputArg) throwErr(new Error('No output path given!'));
 
   let outputFileOrDir = '';
 
   // check args
-  if (configFile && !fs.existsSync(configFile)) utils.throwErr(new Error('config file does not exist'));
-  if (!fs.existsSync(inputFileOrDir)) utils.throwErr(new Error('input file does not exist'));
+  if (configFile && !fs.existsSync(configFile)) throwErr(new Error('config file does not exist'));
+  if (!fs.existsSync(inputFileOrDir)) throwErr(new Error('input file does not exist'));
 
   const isInputDir = fs.statSync(inputFileOrDir).isDirectory();
 
   try {
     if (path.extname(outputArg) === '') {
       if (!fs.existsSync(outputArg)) {
-        utils.throwErr(new Error('output dir does not exist'));
+        throwErr(new Error('output dir does not exist'));
       }
 
       outputFileOrDir = path.resolve(outputArg);
       utils.logInfo('OUTPUT', `Using input file name`);
     } else {
       if (!fs.existsSync(path.dirname(outputArg))) {
-        utils.throwErr(new Error('output dir does not exist'));
+        throwErr(new Error('output dir does not exist'));
       }
 
-      if (isInputDir) utils.throwErr(new Error('output cannot be a file if input is a directory!'));
+      if (isInputDir) throwErr(new Error('output cannot be a file if input is a directory!'));
       outputFileOrDir = outputArg;
       utils.logInfo('OUTPUT', `Using given file name ${outputFileOrDir}`);
     }
   } catch (err) {
-    utils.throwErr(err, true);
+    throwErr(err, true);
   }
   return {
     configFile,
@@ -69,4 +69,19 @@ function parseArgs(argv: string[]) {
     isInputDir,
     outputFileOrDir
   };
+}
+
+function throwErr(err: Error, internal: boolean = false) {
+  if (internal) {
+    console.log(CONST.INTERNAL_ERROR);
+  } else {
+    console.log(err.message, '\n', `Log path: ${CONST.LOG_PATH}`);
+  }
+  fs.appendFileSync(
+    CONST.LOG_PATH,
+    `[${new Date().toISOString()}] ERROR ${err.name} - ${err.message}
+${err.stack || ''}
+`
+  );
+  process.exit(1);
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,21 @@ import * as path from 'path';
 import * as YAML from 'yamljs';
 
 import * as utils from '../utils';
-import { Config, Runner, Input, SectionType, Section, Output, Key, BaseKey, KeyValueKey, TableKey, ListByWordKey, ValueByWordKey, ValueIndexKey } from './config';
+import {
+  Config,
+  Runner,
+  Input,
+  SectionType,
+  Section,
+  Output,
+  Key,
+  BaseKey,
+  KeyValueKey,
+  TableKey,
+  ListByWordKey,
+  ValueByWordKey,
+  ValueIndexKey
+} from './config';
 
 export { Config, Runner, Input, SectionType, Section, Output, Key, BaseKey, KeyValueKey, TableKey, ListByWordKey, ValueByWordKey, ValueIndexKey };
 
@@ -18,14 +32,17 @@ export function load(configPath: string): Config {
   const result = validator.validate('Config', config);
 
   if (!result.valid) {
-    utils.throwErr(new Error('VALIDATION: ' + result.error));
+    throw new Error('VALIDATION: ' + result.error);
   } else {
     utils.logInfo('CONFIG', 'validated config successfully!');
   }
 
-  if (config.runner?.inputDir && ( !existsSync(config.runner.inputDir) || !statSync(config.runner.inputDir).isDirectory() )) utils.throwErr(new Error('input dir does not exist'));
-  if (config.runner?.outputDir && ( !existsSync(config.runner.outputDir) || !statSync(config.runner.outputDir).isDirectory() )) utils.throwErr(new Error('output dir does not exist'));
-  if (config.runner?.archiveDir && ( !existsSync(config.runner.archiveDir) || !statSync(config.runner.archiveDir).isDirectory() )) utils.throwErr(new Error('archive dir does not exist'));
+  if (config.runner?.inputDir && (!existsSync(config.runner.inputDir) || !statSync(config.runner.inputDir).isDirectory()))
+    throw new Error('input dir does not exist');
+  if (config.runner?.outputDir && (!existsSync(config.runner.outputDir) || !statSync(config.runner.outputDir).isDirectory()))
+    throw new Error('output dir does not exist');
+  if (config.runner?.archiveDir && (!existsSync(config.runner.archiveDir) || !statSync(config.runner.archiveDir).isDirectory()))
+    throw new Error('archive dir does not exist');
 
   if (config.runner) {
     config.runner.inputDir = path.resolve(config.runner.inputDir);
