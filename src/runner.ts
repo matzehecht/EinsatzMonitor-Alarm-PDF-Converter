3810161eba59f2d81f184ef36cbf9c904d89db00
Matthias Hecht <matthias@hecht.space>
2020-10-24 01:22:29 +0200

    fix: fixes the fallback mode

--- src/runner.ts
+++ src/runner.ts
@@ -10,19 +10,19 @@ import * as utils from './utils';
 
 const shouldTrace = Boolean(process.env.SHOULD_TRACE) || false;
 
-const adapter = shouldTrace ? new LowDbAdapter({ dbName: path.join(path.dirname(process.execPath), './tmp/perf.json') }) : undefined;
+const adapter = shouldTrace ? new LowDbAdapter({ dbName: path.join(utils.basePath , './tmp/perf.json') }) : undefined;
 if (shouldTrace) TraceIt.init(adapter as LowDbAdapter);
 
-const CONFIG_FILE = path.join(path.dirname(process.execPath), './emapc.conf.yml');
+const CONFIG_FILE = path.join(utils.basePath, './emapc.conf.yml');
 
 const configTransaction = shouldTrace ? TraceIt.startTransaction('loadConfig') : undefined;
 configTransaction?.set('path', CONFIG_FILE);
-if (CONFIG_FILE && !existsSync(CONFIG_FILE)) utils.throwErr(new Error('config file does not exist'));
+if (CONFIG_FILE && !existsSync(CONFIG_FILE)) throw new Error('config file does not exist');
 const config = loadConfig(CONFIG_FILE);
 configTransaction?.set('config', config);
 configTransaction?.end();
 
-if (!config.runner) utils.throwErr(new Error('CONFIG - runner config missing!'));
+if (!config.runner) throw new Error('CONFIG - runner config missing!');
 
 const watcher = chokidar.watch(`${utils.unixPathFrom(config.runner!.inputDir)}/*.pdf`, {
   followSymlinks: false,
@@ -34,12 +34,22 @@ watcher.on('add', run);
 async function run(filepath: string, stat?: Stats) {
   const fileChangeTransaction = shouldTrace ? TraceIt.startTransaction('file change detected') : undefined;
   fileChangeTransaction?.set('filepath', filepath);
-  await convert(filepath, false, config.runner?.outputDir as string, config, fileChangeTransaction);
 
+  try {
+    await convert(filepath, false, config.runner?.outputDir as string, config, fileChangeTransaction);
+  } catch (err) {
+    await archiveFile(filepath, fileChangeTransaction);
+    throw err;
+  }
+
+  await archiveFile(filepath, fileChangeTransaction);
+  fileChangeTransaction?.end();
+}
+
+async function archiveFile(filepath: string, transaction?: TraceIt.Transaction) {
   if (config.runner?.archiveDir) {
-    const archiveTransaction = fileChangeTransaction?.startChild('archive');
+    const archiveTransaction = transaction?.startChild('archive');
     await fsPromises.rename(path.resolve(filepath), path.resolve(config.runner?.archiveDir as string, path.basename(filepath)));
     archiveTransaction?.end();
   }
-  fileChangeTransaction?.end();
 }
